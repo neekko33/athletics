d52214cdd1e278360264f5ae4f74cf9a25ea4b69
neekko33 <neekko33@gmail.com>
2025-10-06 00:56:15 +0800

    增加测试账号显示

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -87,6 +87,11 @@
                             @enderror
                         </div>
 
+                        <div class="text-sm text-gray-400 mt-2">
+                            测试账号: test@example.com
+                            密码: password
+                        </div>
+
                         <!-- 记住我 -->
                         <div class="form-control mt-4">
                             <label class="label cursor-pointer justify-start">
